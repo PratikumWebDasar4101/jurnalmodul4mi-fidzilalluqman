8be2352c6fbd8b7e51cf8cda8a7c80b8dc891758
fidzilalluqman <42725339+fidzilalluqman@users.noreply.github.com>
2018-09-24 10:56:47 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,20 @@
 <?php
+session_start();
 
+$dir = "uploads/";
+$nama_file = $_FILES["filegambar"]["name"];
+$nama_file_tmp = $_FILES["filegambar"]["tmp_name"];
+$target_file = $dir.$nama_file;
+
+if (move_uploaded_file($nama_file_tmp, $target_file)) {
+	echo "Berhasil Upload";
+	header("Location: halamanakhir.php");
+}
+
+$user = array(
+	"hobby" => $_POST["hobby"],
+	"filegambar" => $target_file
+);
+
+$_SESSION["user"] = $user;
 ?>
